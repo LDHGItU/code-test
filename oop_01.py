fce2e58ad23f3cbf298c77d9c1406a81c758947d
253685009@qq.com <253685009@qq.com>
2019-03-18 10:31:55 +0800

    class exercise

---
+++ oop_01.py
@@ -0,0 +1,13 @@
+class Students():
+    name = 'anyone'
+    age = '24'
+    gender = 'male'
+    def work(self):
+        print('do my homework')
+        return None
+
+lee = Students()
+print(lee.name)
+print(lee.age)
+print(lee.gender)
+lee.work()
